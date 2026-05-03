b5b70a4978ea24876c17e9e8163dcb5e85df5163
matrix <markmma1985@gmail.com>
2026-05-03 20:51:45 +1000

    Pre-apply FLASH40 40% coupon to all 20 Stripe payment links sitewide

---
+++ _write_yte.ps1
@@ -0,0 +1,2 @@
+﻿$content = Get-Content "C:\Users\markm\Desktop\freelanceos-storefront\_yte_tmp.txt" -Raw
+Set-Content -Path "C:\Users\markm\Desktop\freelanceos-storefront\youtube-empire.html" -Value $content -Encoding UTF8
